03cff94531afdfda250fb6a36494c2a08bb496fe
myOmikron <git@omikron.dev>
2022-08-02 19:49:50 +0200

    Added option to kill processes that exceeds the Configuration.ProcessTime Limit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/monitoring-agency/q-scheduler
 go 1.18
 
 require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be
 	github.com/hellflame/argparse v1.8.0
 	github.com/labstack/echo/v4 v4.7.2
 	github.com/labstack/gommon v0.3.1
@@ -15,13 +16,9 @@ require (
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/myOmikron/masochism v0.0.0-20220331222721-8612043c2e05 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
--- modules/scheduler/check_task.go
+++ modules/scheduler/check_task.go
@@ -1,25 +1,71 @@
 package scheduler
 
 import (
-	"fmt"
-	"github.com/myOmikron/echotools/utility"
+	"context"
+	"errors"
+	"io/ioutil"
 	"os/exec"
 	"time"
 
+	"github.com/anmitsu/go-shlex"
+	"github.com/myOmikron/echotools/color"
 	"github.com/myOmikron/echotools/worker"
 
 	"github.com/monitoring-agency/q-scheduler/models"
 )
 
-func runCheck(cmdline string) (*models.Result, error) {
+func runCheck(cmdline string, s *scheduler) (*models.Result, error) {
 	start := time.Now()
-	cmd := exec.Command(cmdline)
-	stdout, err := cmd.Output()
+
+	parts, err := shlex.Split(cmdline, true)
 	if err != nil {
 		return nil, err
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(s.configuration.ProcessTimeout)*time.Second)
+	defer cancel()
+
+	args := make([]string, 0)
+	if len(parts) > 1 {
+		args = parts[1:]
+	}
+
+	cmd := exec.CommandContext(ctx, parts[0], args...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return &models.Result{
+			Output:        err.Error(),
+			ReturnCode:    2,
+			ExecutionTime: time.Now().Sub(start),
+		}, nil
+	}
+
+	if err := cmd.Wait(); err != nil {
+		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+			return &models.Result{
+				Output:        "Check took to long to execute",
+				ExecutionTime: time.Now().Sub(start),
+			}, nil
+		}
+		if e, ok := err.(*exec.ExitError); ok {
+			return &models.Result{
+				Output:        e.String(),
+				ReturnCode:    e.ExitCode(),
+				ExecutionTime: time.Now().Sub(start),
+			}, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	output, _ := ioutil.ReadAll(stdout)
 	return &models.Result{
-		Output:        string(stdout),
+		Output:        string(output),
 		ExecutionTime: time.Now().Sub(start),
 		ReturnCode:    cmd.ProcessState.ExitCode(),
 	}, nil
@@ -35,13 +81,15 @@ func createCheckTask(check models.Check, s *scheduler) worker.Task {
 
 					if tp.Start <= currMinute && tp.Stop > currMinute {
 						// Check is allowed to run
-						result, err := runCheck(check.Commandline)
+						result, err := runCheck(check.Commandline, s)
 						if err != nil {
-							fmt.Println("error: " + err.Error())
+							color.Println(color.RED, "error: "+err.Error())
 							return err
 						}
 
-						utility.PPrintln(result)
+						// Fill rest of struct
+						result.CheckID = check.ID
+
 					}
 				}
 			}
